9d1751637ddf05a69343ffc1fec4923ce60bdd7e
elvik11 <elviksharma111@gmail.com>
2022-10-23 15:07:54 +0545

    skeleton page

---
+++ main/main.js
@@ -0,0 +1,22 @@
+// post status
+
+// const input = document.getElementById('staatus-input')
+// const output = document.getElementById('staatus-output')
+// function btn() {
+//     input.innerHTML = output
+
+// }
+
+// like button
+// const btn = document.getElementById('likebtn');
+
+// btn.addEventListener('click', function onClick() {
+//   btn.style.backgroundColor = 'salmon';
+//   btn.style.color = 'white';
+// });
+
+// function func(){
+//     btn.style.backgroundColor = 'salmon';
+//   btn.style.color = 'white';
+// }
+
